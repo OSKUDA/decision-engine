b4264f890a0bfd8d6c3b488423a5161c741ba1c9
OSKUDA <oscarstha@live.com>
2025-12-28 16:34:38 +0530

    initial project setup w/ spring security, spring data, lombok

--- src/main/java/com/example/decision_engine/config/security/SecurityConfig.java
+++ src/main/java/com/example/decision_engine/config/security/SecurityConfig.java
@@ -1,2 +1,20 @@
-package com.example.decision_engine.config.security;public class SecurityConfig {
+package com.example.decision_engine.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .build();
+    }
+
 }
